3e78b64d9d74d43c4aaf8a053596a99c8a72fd1e
Kevin <kevanatole@gmail.com>
2016-05-23 20:12:49 +0100

    Debug

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule SportScore.PageController do
   use SportScore.Web, :controller
 
   def index(conn, _params) do  	
+  	IO.inspect(Application.get_all_env(:addict))
     render conn, "index.html", current_user: get_session(conn, :current_user)
   end
 end
