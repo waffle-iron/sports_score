548463b29ff7ea4fe9b4a5edca02ca73cf30ecf4
Kevin <kevanatole@gmail.com>
2016-05-23 15:09:55 +0100

    empty home

--- web/static/js/vue/components/general/home.vue
+++ web/static/js/vue/components/general/home.vue
@@ -1,8 +1,5 @@
 <template>
-	<h1 class="text-center"> Welcome to sportscore </h1>
-	<div class="jumbotron">
-		<p class="lead"> Here you are able to see the live scores as well as the upcoming fixtures of the gmes </p>
-	</div>
+	
 </template>
 <style type="text/css"></style>
 <script type="text/javascript"></script>
