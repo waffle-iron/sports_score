dacc622ff90879fbe16517ecdb6310d28c919032
Kevin <kevanatole@gmail.com>
2016-06-02 11:37:49 +0100

    testing the unique name_system constraint

--- test/models/sport_test.exs
+++ test/models/sport_test.exs
@@ -2,9 +2,17 @@ defmodule SportScore.SportTest do
   use SportScore.ModelCase
 
   alias SportScore.Sport
+  alias SportScore.User
 
-  @valid_attrs %{name: "some content", name_system: "some content"}
+  @valid_attrs %{name: "some content", name_system: "some content", user_id: 1}
   @invalid_attrs %{}
+
+  setup do
+    userChangeset = User.changeset(%User{}, %{email: "kev@gmil.com", name: "Kevin", encrypted_password: "dsfsdd89afhndsiafnads8csd9"})
+    {:ok, user}  = Repo.insert(userChangeset)
+
+    {:ok, data: user}
+  end
 
   test "changeset with valid attributes" do
     changeset = Sport.changeset(%Sport{}, @valid_attrs)
@@ -14,5 +22,17 @@ defmodule SportScore.SportTest do
   test "changeset with invalid attributes" do
     changeset = Sport.changeset(%Sport{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "name system is unique", %{data: user} do
+    params = Map.put(@valid_attrs, :user_id, user.id )
+    changeset = Sport.changeset(%Sport{}, params)
+    {:ok, firstSport} = Repo.insert(changeset)
+    changeset2 = Sport.changeset(%Sport{}, params)
+    {:error, secondSport} = Repo.insert(changeset2)
+
+    sports = Repo.all(Sport)
+    IO.inspect sports
+    assert(length(sports)  == 1)
   end
 end
