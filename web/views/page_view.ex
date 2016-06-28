8b9e58bdeff4a750d491925e23cabb374e338d7f
Kevin <kevanatole@gmail.com>
2016-06-28 22:34:51 +0100

    Added the son template to render the changes errors (Seems generic enough, might move it to the error view)

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,6 +1,13 @@
 defmodule SportScore.PageView do
   use SportScore.Web, :view
 
+  def render("changesetErrors.json", %{errors: errors}) do
+    #render_many(errors, SportScore.PageView, "changesetError.json")
+    %{
+      success: false,
+      errors: render_many(errors, SportScore.PageView, "changesetError.json", as: :error)
+    }
+  end
 
   def render("user.json", %{user: user}) do
     if is_nil(user) do
@@ -8,5 +15,14 @@ defmodule SportScore.PageView do
     else
       %{id: user.id}
     end
+  end
+
+  def render("newUser.json", %{user: user}) do
+
+  end
+
+  def render("changesetError.json", %{error: error}) do
+    {key, value} = error
+    value
   end
 end
