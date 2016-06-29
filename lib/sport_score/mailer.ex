3f6b00c277e983a2061bb1ede0acc291d92b9218
Kevin <kevanatole@gmail.com>
2016-06-29 11:41:10 +0100

    Mailer library. Copied from the opemaize-phoenix application

---
+++ lib/sport_score/mailer.ex
@@ -0,0 +1,44 @@
+defmodule SportScore.Mailer do
+  @moduledoc """
+  An example module for sending emails.
+  This example uses `mailgun`, but there are also other options available,
+  such as `mailman`.
+  """
+
+  use Mailgun.Client, domain: Application.get_env(:sport_score, :mailgun_domain),
+                      key:    Application.get_env(:sport_score, :mailgun_key)
+
+  @from "contact@sportscore.totole.xyz"
+
+  @doc """
+  An email with a confirmation link in it.
+  """
+  def ask_confirm(email, link) do
+    confirm_url = "http://localhost:4000/confirm?#{link}"
+    send_email to: email,
+              from: @from,
+              subject: "Request confirmation",
+              html: Phoenix.View.render_to_string(SportScore.EmailView, "ask_confirm.html", %{confirm_url: confirm_url})
+  end
+
+  @doc """
+  An email with a link to reset the password.
+  """
+  def ask_reset(email, link) do
+    confirm_url = "http://www.example.com/reset?#{link}"
+    send_email to: email,
+    from: @from,
+    subject: "Reset password",
+    html: Phoenix.View.render_to_string(SportScore.EmailView, "ask_reset.html", %{confirm_url: confirm_url})
+  end
+
+  @doc """
+  An email acknowledging that the account has been successfully confirmed.
+  """
+  def receipt_confirm(email) do
+    send_email to: email,
+              from: @from,
+              subject: "Successful confirmation",
+              html: Phoenix.View.render_to_string(SportScore.EmailView, "receipt_confirm.html", %{})
+  end
+end
