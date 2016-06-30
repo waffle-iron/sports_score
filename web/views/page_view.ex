471e4816919a0d8b68471e2748736337f7889ab0
Kevin <kevanatole@gmail.com>
2016-06-30 07:13:41 +0100

    Moved the templates to the proper views

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,28 +1,11 @@
 defmodule SportScore.PageView do
   use SportScore.Web, :view
 
-  def render("changesetErrors.json", %{errors: errors}) do
-    #render_many(errors, SportScore.PageView, "changesetError.json")
-    %{
-      success: false,
-      errors: render_many(errors, SportScore.PageView, "changesetError.json", as: :error)
-    }
-  end
-
   def render("user.json", %{user: user}) do
     if is_nil(user) do
       %{id: 0}
     else
       %{id: user.id}
     end
-  end
-
-  def render("newUser.json", %{user: user}) do
-
-  end
-
-  def render("changesetError.json", %{error: error}) do
-    {key, value} = error
-    value
   end
 end
